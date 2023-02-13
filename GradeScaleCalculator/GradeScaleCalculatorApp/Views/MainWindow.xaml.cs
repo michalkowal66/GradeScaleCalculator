146bb30994c90c1abf17b65fcd4f3a8f45d1143b
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-13 12:30:15 +0100

    Bind main view model to the main window data context

--- GradeScaleCalculator/GradeScaleCalculatorApp/Views/MainWindow.xaml.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/Views/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GradeScaleCalculatorApp.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GradeScaleCalculatorApp
 {
@@ -20,8 +9,13 @@ namespace GradeScaleCalculatorApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel _viewModel;
+
         public MainWindow()
         {
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
+
             InitializeComponent();
         }
 
