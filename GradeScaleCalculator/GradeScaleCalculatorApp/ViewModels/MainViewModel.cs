dbdca9d914329bdaa0388dd1077860f0b58943a8
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-13 12:23:24 +0100

    Add empty main view model

---
+++ GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace GradeScaleCalculatorApp.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+   
+    }
+}
