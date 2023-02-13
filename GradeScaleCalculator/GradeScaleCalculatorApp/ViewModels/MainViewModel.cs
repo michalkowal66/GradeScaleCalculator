7dc5868741a7f62d891b2fdd0a4ecb5baaf6f6db
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-13 12:25:25 +0100

    Add main view model view presentation related properties

--- GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/MainViewModel.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/MainViewModel.cs
@@ -5,6 +5,19 @@ namespace GradeScaleCalculatorApp.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
+        private ObservableObject _currentViewModel;
+
+        public HomeViewModel HomeViewModel { get; } = new();
+
+        public EditViewModel EditViewModel { get; } = new();
+
+        public InfoViewModel InfoViewModel { get; } = new();
+
+        public ObservableObject CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value);
+        }
    
     }
 }
