b925e1c37d02f2bc364d55ffbb6a64ff8a45c9ab
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-19 22:44:08 +0100

    Main vm add dummy grading scales property, initialize vm in cosntructor

--- GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/MainViewModel.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using GradeScaleCalculatorApp.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GradeScaleCalculatorApp.ViewModels
 {
@@ -7,11 +10,13 @@ namespace GradeScaleCalculatorApp.ViewModels
     {
         private ObservableObject _currentViewModel;
 
-        public HomeViewModel HomeViewModel { get; } = new();
+        private ObservableCollection<GradingScale> _gradingScales;
 
-        public EditViewModel EditViewModel { get; } = new();
+        public HomeViewModel HomeViewModel { get; }
 
-        public InfoViewModel InfoViewModel { get; } = new();
+        public EditViewModel EditViewModel { get; }
+
+        public InfoViewModel InfoViewModel { get; }
 
         public ObservableObject CurrentViewModel
         {
@@ -19,8 +24,24 @@ namespace GradeScaleCalculatorApp.ViewModels
             set => SetProperty(ref _currentViewModel, value);
         }
 
+        public ObservableCollection<GradingScale> GradingScales
+        {
+            get => _gradingScales;
+            set => SetProperty(ref _gradingScales, value);
+        }
+
         public MainViewModel()
         {
+            GradingScales = new ObservableCollection<GradingScale>() 
+            { 
+                new GradingScale(), 
+                new GradingScale() { Name = "Domyślna skala 2", AGrade = new GradeRange(90, 100), BGrade = new GradeRange(85, 90) } 
+            };
+
+            HomeViewModel = new(GradingScales);
+            EditViewModel = new(GradingScales);
+            InfoViewModel = new();
+
             CurrentViewModel = HomeViewModel;
 
             ChangeViewModelCommand = new RelayCommand<ObservableObject>(ChangeViewModel);
