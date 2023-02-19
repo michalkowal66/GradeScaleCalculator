35284c9b616d5a701a39f05aeaf46da62b7198e3
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-19 21:19:01 +0100

    Add home view model properties and constructor

--- GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/HomeViewModel.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/HomeViewModel.cs
@@ -1,9 +1,60 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GradeScaleCalculatorApp.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GradeScaleCalculatorApp.ViewModels
 {
     public class HomeViewModel : ObservableObject
     {
+        private ObservableCollection<GradingScale> _gradingScales;
 
+        private GradingScale _currentGradingScale;
+
+        private Scoring _calculatedScoring;
+
+        private int _maxPoints = 100;
+
+        public ObservableCollection<GradingScale> GradingScales
+        {
+            get => _gradingScales;
+            set => SetProperty(ref _gradingScales, value);
+        }
+
+        public GradingScale CurrentGradingScale
+        {
+            get => _currentGradingScale;
+            set
+            {
+                if (SetProperty(ref _currentGradingScale, value))
+                {
+                    CalculatedScoring = Scoring.FromScale(value, MaxPoints);
+                }
+            }
+        }
+
+        public Scoring CalculatedScoring
+        {
+            get => _calculatedScoring;
+            set => SetProperty(ref _calculatedScoring, value);
+        }
+
+        public int MaxPoints
+        {
+            get => _maxPoints;
+            set
+            {
+                if (SetProperty(ref _maxPoints, value))
+                {
+                    CalculatedScoring = Scoring.FromScale(CurrentGradingScale, value);
+                }
+            }
+        }
+
+        public HomeViewModel(ObservableCollection<GradingScale> gradingScales)
+        {
+            GradingScales = gradingScales;
+            CurrentGradingScale = GradingScales.First();
+        }
     }
 }
