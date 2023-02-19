30d7e7f54ccf91dcb8cc20c920b1c5f11ec46aef
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-19 21:17:51 +0100

    Add edit view model properties and constructor

--- GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/EditViewModel.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/ViewModels/EditViewModel.cs
@@ -1,9 +1,46 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GradeScaleCalculatorApp.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GradeScaleCalculatorApp.ViewModels
 {
     public class EditViewModel : ObservableObject
     {
+        private ObservableCollection<GradingScale> _gradingScales;
 
+        private GradingScale _currentGradingScale;
+
+        private GradingScale _editedGradingScale;
+
+        public ObservableCollection<GradingScale> GradingScales
+        {
+            get => _gradingScales;
+            set => SetProperty(ref _gradingScales, value);
+        }
+
+        public GradingScale CurrentGradingScale
+        {
+            get => _currentGradingScale;
+            set
+            {
+                SetProperty(ref _currentGradingScale, value);
+
+                if (value != null) EditedGradingScale = value.Clone();
+            }
+        }
+
+        public GradingScale EditedGradingScale
+        {
+            get => _editedGradingScale;
+            set => SetProperty(ref _editedGradingScale, value);
+        }
+
+        public EditViewModel(ObservableCollection<GradingScale> gradingScales)
+        {
+            GradingScales = gradingScales;
+            CurrentGradingScale = GradingScales.First();
+        }
     }
 }
