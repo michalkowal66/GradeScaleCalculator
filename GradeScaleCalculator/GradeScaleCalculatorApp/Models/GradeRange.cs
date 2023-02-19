0798c82ee8e1e576f944e4f3f1b7b3689da3a588
Michal Kowal <michal.kowal.66@gmail.com>
2023-02-19 21:13:25 +0100

    Add grade range model

---
+++ GradeScaleCalculator/GradeScaleCalculatorApp/Models/GradeRange.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace GradeScaleCalculatorApp.Models
+{
+    public class GradeRange : ObservableObject
+    {
+        private double _gradeMin;
+        private double _gradeMax;
+
+        public double GradeMin
+        {
+            get => Math.Round(_gradeMin);
+            set => SetProperty(ref _gradeMin, value);
+        }
+
+        public double GradeMax
+        {
+            get => Math.Round(_gradeMax);
+            set => SetProperty(ref _gradeMax, value);
+        }
+
+        public GradeRange(double gradeMin, double gradeMax) 
+        {
+            GradeMin = gradeMin;
+            GradeMax = gradeMax;
+        }
+    }
+}
