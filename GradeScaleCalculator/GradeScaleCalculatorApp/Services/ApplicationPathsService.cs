6e16b396d350ea4f71cd30a7292ac95bce137282
Michal Kowal <michal.kowal.66@gmail.com>
2023-03-11 13:11:17 +0100

    Add application paths service, app settings folder and file get methods

---
+++ GradeScaleCalculator/GradeScaleCalculatorApp/Services/ApplicationPathsService.cs
@@ -0,0 +1,27 @@
+﻿using static GradeScaleCalculatorApp.Models.Statics;
+using System.IO;
+
+namespace GradeScaleCalculatorApp.Services
+{
+    internal static class ApplicationPathsService
+    {
+        public static string GetAppSettingsFolder()
+        {
+            string settingsFolder = Path.Combine(AppSettingsRootFolder, AppName);
+
+            if (!Directory.Exists(settingsFolder)) Directory.CreateDirectory(settingsFolder);
+
+            return settingsFolder;
+        }
+
+        public static string GetGradingScalesSettingsFile() 
+        {
+            string appSettingsFolder = GetAppSettingsFolder();
+
+            string gradingScalesSettingsFile = Path.Combine(appSettingsFolder, GradingScalesSettingsFilename);
+            if (!File.Exists(gradingScalesSettingsFile)) File.Create(gradingScalesSettingsFile);
+
+            return gradingScalesSettingsFile;
+        }
+    }
+}
