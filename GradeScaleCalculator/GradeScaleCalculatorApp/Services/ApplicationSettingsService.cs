eaeeb605461d7fad0261da47c65c2c01e6cee0ed
Michal Kowal <michal.kowal.66@gmail.com>
2023-03-11 15:32:47 +0100

    Rename application paths service to application settings service

--- GradeScaleCalculator/GradeScaleCalculatorApp/Services/ApplicationSettingsService.cs
+++ GradeScaleCalculator/GradeScaleCalculatorApp/Services/ApplicationSettingsService.cs
@@ -1,9 +1,13 @@
 ﻿using static GradeScaleCalculatorApp.Models.Statics;
 using System.IO;
+using System.Text;
+using System.Collections;
+using GradeScaleCalculatorApp.Models;
+using System.Collections.Generic;
 
 namespace GradeScaleCalculatorApp.Services
 {
-    internal static class ApplicationPathsService
+    internal static class ApplicationSettingsService
     {
         public static string GetAppSettingsFolder()
         {
@@ -14,7 +18,7 @@ namespace GradeScaleCalculatorApp.Services
             return settingsFolder;
         }
 
-        public static string GetGradingScalesSettingsFile() 
+        public static string GetGradingScalesSettingsFile()
         {
             string appSettingsFolder = GetAppSettingsFolder();
 
