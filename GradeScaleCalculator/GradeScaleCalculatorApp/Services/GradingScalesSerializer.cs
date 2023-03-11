891f74683b8812a021d1585c6a8e0cd71b7dee51
Michal Kowal <michal.kowal.66@gmail.com>
2023-03-11 13:32:16 +0100

    Add grading scales serializer service

---
+++ GradeScaleCalculator/GradeScaleCalculatorApp/Services/GradingScalesSerializer.cs
@@ -0,0 +1,23 @@
+﻿using GradeScaleCalculatorApp.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace GradeScaleCalculatorApp.Services
+{
+    internal static class GradingScalesSerializer
+    {
+        public static string SerializeScales(IEnumerable<GradingScale> gradingScale)
+        {
+            string serializedGradingScales = JsonConvert.SerializeObject(gradingScale);
+
+            return serializedGradingScales;
+        }
+
+        public static IEnumerable<GradingScale>? DeserializeScales(string serializedGradingScales) 
+        {
+            IEnumerable<GradingScale>? deserializedGradingScales = JsonConvert.DeserializeObject<IEnumerable<GradingScale>>(serializedGradingScales);
+
+            return deserializedGradingScales;
+        }
+    }
+}
